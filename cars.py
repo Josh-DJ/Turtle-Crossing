0c4cb0ae9d235f0566084c153e1bb12020e4748b
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-27 13:55:42 -0500

    Created car move functionality

---
+++ cars.py
@@ -0,0 +1,23 @@
+import random
+from turtle import Turtle
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+class Car:
+    def __init__(self):
+        super().__init__()
+        self.cars = []
+
+    def create(self):
+        new_car = Turtle("square")
+        y_axis = random.randint(-260, 260)
+        new_car.shapesize(stretch_len=3)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.goto(325, y_axis)
+        self.cars.append(new_car)
+
+    def move(self):
+        for car in self.cars:
+            car.backward(STARTING_MOVE_DISTANCE)
