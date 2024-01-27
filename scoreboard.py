69f09533fdaa7212302c52b8e44d85751a77bc9a
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-27 16:52:39 -0500

    Added update score and game over functions.

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,32 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+
+class Scoreboard(Turtle):
+
+    # Creates scoreboard.
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-290, 260)
+        self.write("Level: ", font=FONT)
+        self.goto(-180, 260)
+        self.write(self.level, font=FONT)
+
+    # Updates score when new level is reached.
+    def update(self):
+        self.clear()
+        self.level += 1
+        self.goto(-290, 260)
+        self.write("Level: ", font=FONT)
+        self.goto(-180, 260)
+        self.write(self.level, font=FONT)
+
+
+    #Game over screen.
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game over.", align="center", font=FONT)
