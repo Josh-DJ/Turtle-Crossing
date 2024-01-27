67cf7eebdbee615f7d5a1865e0502337a4c27893
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-27 13:57:00 -0500

    Implemented screen and game loop.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from turtle import Screen
 import time
-
+from turtle import Screen
+from player import Player
+from cars import Car
 
 screen = Screen()
 screen.setup(600, 600)
-screen.tracer(0)
 screen.bgcolor("white")
 screen.title("Turtle Crossing")
+screen.tracer(0)
 
-
-
-
-
+turtle = Player()
+car = Car()
+screen.listen()
+screen.onkeypress(key="Up", fun=turtle.move)
 
 game_on = True
-screen.exitonclick()
-
 while game_on:
     time.sleep(0.1)
     screen.update()
+
+    car.create()
+    car.move()
+
+screen.exitonclick()
